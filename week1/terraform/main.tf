589f50d2d6ccb033fb4623ff925ad420b3c27f05
igor-yamamoto <igorhc.yamamoto@gmail.com>
2023-01-22 22:10:32 -0300

    Commiting last changes to push to github

--- week1/terraform/main.tf
+++ week1/terraform/main.tf
@@ -41,3 +41,39 @@ resource "google_bigquery_dataset" "dataset" {
     project = var.project
     location = var.region
 }
+
+resource "google_compute_address" "static_ip" {
+    name = var.compute_static_ip
+}
+
+resource "google_compute_instance" "compute_instance" {
+    name = var.compute_name
+    machine_type = var.compute_machine
+    zone = var.zone
+
+    allow_stopping_for_update = true
+
+    boot_disk {
+        initialize_params {
+            image = var.ubuntu_2004_sku
+            size = 30
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config { 
+            nat_ip = "${google_compute_address.static_ip.address}"
+        } ## needed to enable external IP
+    }
+
+    service_account {
+        scopes = ["cloud-platform"]
+    }
+
+    metadata = {
+        "ssh-keys" = <<EOT
+            igoryamamoto:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcWYQTVpMHA6Mjn3MxnhyE+2tYDH72CWELj2BWvL1jGxRG9x/H/Gi4qzpBcVQOLsoFYCfWrk+fgAHGn55B8tb2nv/W7qN0yNEHcLVXz5o3OhiM/fjgQwG0eRVb9GttSzR1knyKEqkUHFcxXmKemqml88XzsTZ4PUkQc2QW0CK/UPWD/FOcA2y2PZmbQZfcg6gDh2gH76itXZ2QR2Cuy1feUTmNI/bG0djIF9QKeDPgM0lpqvaqIK1d+o1CgMwAmHdsxiVJ434THNmo6aevJdBtC0tOv+RSvtm6KW5J1WuBM9Zd9A0TQjmxrBWcW+1MmJUY2yCz45fL402s+FdotV/j igoryamamoto
+        EOT
+  }
+}
--- week1/terraform/variables.tf
+++ week1/terraform/variables.tf
@@ -13,11 +13,20 @@ variable "region" {
     description = "Region for GCP resources." 
 }
 
+variable "zone" {
+    type = string
+    default = "us-central1-a"
+    description = "Zone for GCP resources."
+}
+
+## GCS variables
 variable "storage_class" {
     description = "Storage class type for the bucket in GCS" 
     default = "STANDARD" 
 }
 
+
+## BigQuery ariables
 variable "BG_DATASET" {
     description = "Raw dataset (from GCS) that will be written into BigQuery"
     type = string
@@ -28,4 +37,34 @@ variable "TABLE_NAME" {
     default = "ny_trips"
     type = string
     description = "BigQuery table"
+}
+
+## compute variables
+variable "compute_name" {
+    default = "de-zoomcamp"
+    type = string
+    description = "Name of the Compute Instance"
+}
+
+variable "compute_machine" {
+    default = "e2-micro"
+    type = string
+    description = "Machine for the Compute Instance"
+}
+
+variable "ubuntu_2004_sku" {
+  type        = string
+  description = "SKU for Ubuntu 20.04 LTS"
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+}
+
+variable "compute_disk_size" {
+    default = 30
+    description = "Disk size for the Compute Instance"
+}
+
+variable "compute_static_ip" {
+    default = "static-ip"
+    type = string
+    description = "Name of GCP Cloud Address instance"
 }
